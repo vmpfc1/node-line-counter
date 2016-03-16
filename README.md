8667ac9fa09e69ea19cbdf6f5f777bf4ed71047c
cameron-gourmetjs <cameron@gourmetjs.com>
2016-03-15 17:55:38 -0700

    original version

---
+++ README.md
@@ -0,0 +1,43 @@
+# node-line-counter
+A simple command line tool to count the number of lines of code in a given directory. node-line-counter searches recursively and is provided with file types and pathways to use as inclusion/exclusion criteria.
+
+## installation
+Install node-line-conter with the following command:
+```
+figure out how to actually do this part !!!!
+```
+
+## usage
+```
+node node-line [directory] [configuration]
+```
+Configuration is .json file formatted like below:
+```
+{
+  "folders": [
+    "!**/node_modules",
+    "!file-runtime/runtime",
+    "!spec-sketch"
+  ],
+  "files": [
+    "**/*.js",
+    "!**/temp.js"
+  ]
+}
+
+```
+
+## example usage
+
+```
+$ node node-line . 'config.js'
+3524 lines in 167 files
+```
+
+## author
+
+Cameron Smith, Gourmet. [cameron@gourmetjs.com](cameron@gourmetjs.com)
+
+## license
+
+MIT
---
+++ read-lines.js
@@ -0,0 +1,118 @@
+var fs = require("fs");
+var mm = require("micromatch");
+var async = require("async");
+
+var rootLocation = process.argv[2];
+var configLocation = process.argv[3];
+
+function grabInput (input, cb) {
+  fs.readFile(input, /*'utf8',*/ function (err, data) {
+
+    // change the names of these variables
+    stuff = [];
+    moreStuff = [];
+    onlyExcludeFiles = [];
+
+    if (err) throw err;
+    var jsonFile = JSON.parse(data);
+    var fileArray = stuff.concat(jsonFile["files"], jsonFile["folders"]);
+    var onlyExcludeFiles = moreStuff.concat(jsonFile["folders"], "*");
+
+    cb(fileArray, onlyExcludeFiles);
+  });
+}
+
+grabInput(configLocation, function(includeFiles, excludeFiles) {
+  var allFiles = 0;
+  var numLines = 0;
+
+  // Checks to see if a path+file is a dir.
+  // Then runs scanDir recursively if path+file is non-excluded.
+  var itemRead = function (path, file, itemReadCb) {
+    function cb(err, stats) {
+      if (err) {
+        itemReadCb()
+        return console.error(err);
+      }
+      // If stats.isDir = true
+      if (!err && stats.isDirectory()) {
+      	// Certain folders are excluded from being scanned.
+        if (mm((file), excludeFiles).length > 0) { 
+          scanDir(path + "/" + file, function() {
+            itemReadCb()
+          });
+
+          //console.log(path + "/" + file + "  <--- this is a folder");
+        }
+        else {
+          itemReadCb()
+        }
+      }
+      else {
+        itemReadCb()
+      }
+    }
+    var filePath = path + "/" + file;
+    fs.stat(filePath, cb);
+  }
+
+  // Reads the current directory, adding the number of lines in each file we care about to numLines
+  function scanDir(path, scanDirCb) {
+    function readFiles(err, files, readFilesCb) {
+      if (err) {
+        return console.error(err, "err1");
+      }
+      if (!err) {
+        // mm discards files that don't match our inclusion criteria
+        var validFiles = mm((files), includeFiles);
+
+        // Reads the number of lines from each file, producing a callback when complete
+        function readLines(file, readLinesCb) {
+          var filePath = path + "/" + file;
+          fs.readFile(filePath, function(err, data) {
+            if (data != undefined) {
+              var array = data.toString().split("\n");
+              numLines += array.length;
+              allFiles+=1;
+            }
+            if (readLinesCb) readLinesCb()
+          });
+        }
+
+        // Run readLines on each thing in validFiles, with cb of 'done'.
+        async.each(validFiles, readLines, function() {
+          //console.log("done")
+          // Once every file is read, itemRead checks each file to see whether its a dir we care about
+          async.each(files, function(item, itemReadCb){
+            // itemRead is called after completion of all readLines
+            itemRead(path, item, itemReadCb);
+          },
+          // callback once every file and dir in current path has been read
+          function(){
+            //console.log("done reading items at", path)
+            if (readFilesCb) readFilesCb()
+          })
+        })					
+      }
+    }
+
+    // fs.readdir reads directory, its callback contains array of all files.
+    // Then readFiles triggers. When readFiles completes, its cb is ScanDir again.
+    fs.readdir(path, function(err, fileArray) {
+      readFiles(err, fileArray, function(){
+        //console.log("done reading files in", path)
+        scanDirCb()
+      })
+    });
+  }
+
+  scanDir(rootLocation, function() {
+    //console.log("done reading directory");
+    console.log("The total number of lines is " + numLines);
+  });
+});
+
+///////////////////////////////////////////////////////////////////////
+// Optimizations - don't stat everything passed to itemRead - find a way to detect all directories in dir. 
+// make functions more readable
+// need to handle errors in fs callbacks, in particular in Readfiles, readLines, readdir
